4fc0505326eff025e075ea2d8fd5ca717d8610b5
Yelkira <email@email.com>
2023-01-08 21:17:37 +0300

    input done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Input} from "./components/Input";
+import {Button} from "./components/Button";
 
 function App() {
+    const [message, setMessage] = useState([
+            {message: 'message1'},
+            {message: 'message2'},
+            {message: 'message3'},
+            {message: 'message4'},
+            {message: 'message5'}
+        ]
+    )
+    let [title, setTitle] = useState("")
+    console.log(title)
+    const addMessage = (title: string) => {
+        let newMessage = {message: title}
+        setMessage([newMessage, ...message])
 
+    }
+    const callBackButtonHandler =()=>{
+        addMessage(title)
+        setTitle("")
+    }
+    return (
+        <div className="App">
+            <Input setTitle={setTitle} title={title}/>
+            <Button name={"+"} callBack={callBackButtonHandler}/>
+            {/*<FullInput addMessage={addMessage}/>*/}
+            {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
+        </div>
+    );
 }
+
 
 export default App;
